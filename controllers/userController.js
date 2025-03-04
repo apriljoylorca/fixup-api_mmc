cb527529dd5ec2428feca3d136681bfa14f0c75d
apriljoylorca <apriljoylorca@gmail.com>
2025-03-04 23:04:09 +0800

    Add User registration API

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+    res.status(201).json({ message: 'User registered', user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/*const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const addUser = async (req, res) => {
+  // Similar to register but for admin use
+};
+
+const updateUser = async (req, res) => {
+  // Update user logic
+};
+
+const deleteUser = async (req, res) => {
+  // Delete user logic
+};
+*/
+module.exports = { register, login, addUser, updateUser, deleteUser };
